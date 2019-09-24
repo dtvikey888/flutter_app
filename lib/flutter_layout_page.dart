7623d9cf690484b4c8bf3f3045e8e7df3a9df610
dtvikey888 <dtvikey@163.com>
2019-09-24 10:13:27 +0800

    first demo

---
+++ lib/flutter_layout_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/main.dart';
+
+class LessGroupPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '12',
+      theme: ThemeData(
+
+      ),
+      home: Scaffold(
+        appBar:AppBar(title: Text('123'),),
+      ),
+    );
+  }
+}
